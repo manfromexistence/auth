967af60bc0c872f4ad27aa5d5e704224e8dceb8a
manfromexistence <ajju40959@gmail.com>
2024-10-19 02:07:56 +0000

    feat: checkpoint

--- apps/www/components/authentication-03.tsx
+++ apps/www/components/authentication-03.tsx
@@ -190,55 +190,131 @@ export default function Authentication03() {
 
   return (
     <>
-      <Tabs defaultValue="account" className="w-[400px]">
-        <TabsContent value="account">
-          <Card>
+      <Tabs defaultValue="web2" className="w-[375px]">
+        <TabsContent value="web2">
+          <Card className="mx-auto max-w-[400px] space-x-1 p-0">
             <CardHeader>
-              <CardTitle>Account</CardTitle>
-              <CardDescription>
-                Make changes to your account here. Click save when you&apos;re done.
+              <Image
+                className="mx-auto rounded-full border"
+                src="/manfromexistence.png"
+                alt="Auth"
+                width={75}
+                height={75}
+              />
+              <CardTitle className="w-full text-center text-4xl">Login</CardTitle>
+              <CardDescription className="w-full text-center ">
+                Enter your email below to login to your account.
               </CardDescription>
             </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" defaultValue="Pedro Duarte" />
+            <CardContent className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="manfromexistence@example.com"
+                  required
+                />
               </div>
-              <div className="space-y-1">
-                <Label htmlFor="username">Username</Label>
-                <Input id="username" defaultValue="@peduarte" />
+              <div className="grid gap-2">
+                <Label htmlFor="current_password">Password</Label>
+                <PasswordInput
+                  placeholder="password"
+                  id="current_password"
+                  value={currentPassword}
+                  onChange={(e) => setCurrentPassword(e.target.value)}
+                  autoComplete="current-password"
+                />
               </div>
             </CardContent>
-            <CardFooter>
-              <Button>Save changes</Button>
+            <CardFooter className="m-0 flex flex-col gap-2">
+              <Button className="w-full">Sign in</Button>
+
+            </CardFooter>
+          </Card>
+
+        </TabsContent>
+        <TabsContent value="web3">
+          <Card className="mx-auto max-w-[400px] space-x-1 p-0">
+            <CardHeader>
+              <Image
+                className="mx-auto rounded-full border"
+                src="/manfromexistence.png"
+                alt="Auth"
+                width={75}
+                height={75}
+              />
+              <CardTitle className="w-full text-center text-4xl">Login</CardTitle>
+              <CardDescription className="w-full text-center ">
+                Select your web2 provider to login to your account.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Command className="max-w-full rounded-lg border shadow-md">
+                <CommandInput placeholder="Type a provider or search..." />
+                <CommandList>
+                  <CommandEmpty>No results found.</CommandEmpty>
+                  <CommandGroup heading="Suggestions">
+                    <CommandItem>
+                      <Calendar className="mr-2 h-4 w-4" />
+                      <span>Calendar</span>
+                    </CommandItem>
+                    <CommandItem>
+                      <Smile className="mr-2 h-4 w-4" />
+                      <span>Search Emoji</span>
+                    </CommandItem>
+                    <CommandItem disabled>
+                      <Calculator className="mr-2 h-4 w-4" />
+                      <span>Calculator</span>
+                    </CommandItem>
+                  </CommandGroup>
+                  <CommandSeparator />
+                  <CommandGroup heading="Settings">
+                    <CommandItem>
+                      <User className="mr-2 h-4 w-4" />
+                      <span>Profile</span>
+                      <CommandShortcut>⌘P</CommandShortcut>
+                    </CommandItem>
+                    <CommandItem>
+                      <CreditCard className="mr-2 h-4 w-4" />
+                      <span>Billing</span>
+                      <CommandShortcut>⌘B</CommandShortcut>
+                    </CommandItem>
+                    <CommandItem>
+                      <Settings className="mr-2 h-4 w-4" />
+                      <span>Settings</span>
+                      <CommandShortcut>⌘S</CommandShortcut>
+                    </CommandItem>
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </CardContent>
+            <CardFooter className="m-0 flex flex-col gap-2">
             </CardFooter>
           </Card>
         </TabsContent>
-        <TabsContent value="password">
-          <Card>
-            <CardHeader>
-              <CardTitle>Password</CardTitle>
-              <CardDescription>
-                Change your password here. After saving, you&apos;ll be logged out.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="current">Current password</Label>
-                <Input id="current" type="password" />
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="new">New password</Label>
-                <Input id="new" type="password" />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button>Save password</Button>
-            </CardFooter>
-          </Card>
-        </TabsContent>
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="account">Account</TabsTrigger>
+        <TabsList className="supports-backdrop-blur:bg-white/10 supports-backdrop-blur:dark:bg-black/10 mx-auto mt-2 flex h-[58px] w-full grid-cols-2 items-center justify-center gap-2 rounded-2xl border p-2 backdrop-blur-md">
+          <TabsTrigger value="web2">
+            <Earth className="h-4 w-4" />
+          </TabsTrigger>
+          <TabsTrigger value="web3">
+            <EarthLock className="h-4 w-4" />
+          </TabsTrigger>
+          <TabsTrigger value="qr-code">
+            <QrCode className="h-4 w-4" />
+          </TabsTrigger>
+          <TabsTrigger value="auth-code">
+            <Key className="h-4 w-4" />
+          </TabsTrigger>
+          <TabsTrigger value="biometric">
+            <Fingerprint className="h-4 w-4" />
+          </TabsTrigger>
+          <TabsTrigger value="pattern">
+            <Shapes className="h-4 w-4" />
+          </TabsTrigger>
+          <TabsTrigger value="qna">
+            <NotebookTabs className="h-4 w-4" />
+          </TabsTrigger>
         </TabsList>
       </Tabs>
 
